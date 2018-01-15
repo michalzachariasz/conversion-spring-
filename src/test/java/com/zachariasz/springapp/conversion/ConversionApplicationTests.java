92e81ae2cecb2682239c84913e630a31c5c3dd14
Michał Zachariasz <michal.zachariasz1990@gmail.com>
2018-01-15 21:24:39 +0100

    prototype version .01

--- src/test/java/com/zachariasz/springapp/conversion/ConversionApplicationTests.java
+++ src/test/java/com/zachariasz/springapp/conversion/ConversionApplicationTests.java
@@ -1,65 +1,207 @@
 package com.zachariasz.springapp.conversion;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zachariasz.springapp.conversion.exception.IncorrectInputDataException;
+import com.zachariasz.springapp.conversion.message.ExceptionHandlingMessages;
 import com.zachariasz.springapp.conversion.model.Exchange;
-import com.zachariasz.springapp.conversion.service.ExchangeService;
+import com.zachariasz.springapp.conversion.repository.ExchangeRepository;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultHandler;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
 
 
-import static org.junit.Assert.assertEquals;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.core.Is.is;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ConversionApplicationTests {
-	private WebApplicationContext webApplicationContext;
-	private ExchangeService exchangeService;
-
-	@Autowired
-	public void setWebApplicationContext(WebApplicationContext webApplicationContext) {
-		this.webApplicationContext = webApplicationContext;
-	}
-	@Autowired
-	public void setExchangeService(ExchangeService exchangeService) {
-		this.exchangeService = exchangeService;
-	}
-
-	MockMvc mockMvc;
-
-	@Before
-	public void setUp(){
-		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-	}
-
-	@Test
-	public void checkExchangeTest(){
-		checkExchangeTestingMethod(new Exchange("USD", 2.0), "");
-		checkExchangeTestingMethod(new Exchange("USD", 0.0), "VALUE should not: be zero");
-		checkExchangeTestingMethod(new Exchange("USD", null), "VALUE cannot be null");
-		checkExchangeTestingMethod(new Exchange("US", 2.0), "ID should contain: 3 chars");
-		checkExchangeTestingMethod(new Exchange("U@", 2.0), "ID should contain: 3 chars, only a-z or A-Z letters");
-		checkExchangeTestingMethod(new Exchange("U@", 0.0), "ID should contain: 3 chars, only a-z or A-Z letters VALUE should not: be zero");
-		checkExchangeTestingMethod(new Exchange("U@", null), "ID should contain: 3 chars, only a-z or A-Z letters VALUE cannot be null");
-		checkExchangeTestingMethod(new Exchange(null, 0.0), "ID cannot be null VALUE should not: be zero");
-	}
+    private String baseExchange = "PLN";
+    private WebApplicationContext webApplicationContext;
+    private ExchangeRepository exchangeRepository;
+    private List<Exchange> correctExchanges = new ArrayList<>();
+    private List<Exchange> incorrectExchanges = new ArrayList<>();
 
 
+    @Autowired
+    public void setWebApplicationContext(WebApplicationContext webApplicationContext, ExchangeRepository exchangeRepository) {
+        this.webApplicationContext = webApplicationContext;
+        this.exchangeRepository = exchangeRepository;
+    }
 
-	private void checkExchangeTestingMethod(Exchange exchange, String desiredResult) {
-		String result = exchangeService.checkExchange(exchange);
-		assertEquals(desiredResult, result);
-	}
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Before
+    public void addSampleValuesIdCannotBeBaseExchange() throws IncorrectInputDataException {
+        // object new Exchange(baseExchange,....) is forbidden in correctExchanges !
+        correctExchanges.add(new Exchange("USD", 3.231));
+        correctExchanges.add(new Exchange("EUR", 4.135));
+        correctExchanges.add(new Exchange("CFK", 2.273));
+        correctExchanges.add(new Exchange("RFS", 0.731));
+
+        incorrectExchanges.add(new Exchange("US", 1.0));
+        incorrectExchanges.add(new Exchange("US@", 1.0));
+        incorrectExchanges.add(new Exchange("U", 1.0));
+        incorrectExchanges.add(new Exchange("@", 1.0));
+        incorrectExchanges.add(new Exchange("", 1.0));
+        incorrectExchanges.add(new Exchange("USAD", 1.0));
+        incorrectExchanges.add(new Exchange(null, 1.0));
+        incorrectExchanges.add(new Exchange(null, null));
+        incorrectExchanges.add(new Exchange("US", 0.0));
+        incorrectExchanges.add(new Exchange("US@", 0.0));
+        incorrectExchanges.add(new Exchange("U", 0.0));
+        incorrectExchanges.add(new Exchange("@", 0.0));
+        incorrectExchanges.add(new Exchange("", 0.0));
+        incorrectExchanges.add(new Exchange("USAD", 0.0));
+        incorrectExchanges.add(new Exchange("USD", null));
+        incorrectExchanges.add(new Exchange("USD", 0.0));
+        incorrectExchanges.add(new Exchange("PLN", 1.0));
+        incorrectExchanges.add(new Exchange());
+        incorrectExchanges.add(null);
+
+        addSampleValuesToDatabase();
+    }
+
+    @After
+    public void removeAllSampleValues() {
+        correctExchanges.clear();
+        exchangeRepository.deleteAll();
+        incorrectExchanges.clear();
+    }
+
+
+    @Test//finished
+    public void addExchangeTest() throws Exception {
+
+        for (Exchange correctExchange : correctExchanges) {
+            mockMvc.perform(post("/exchanges")
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .content(asJsonString(correctExchange)))
+                    .andExpect(status().isCreated())
+                    .andExpect(jsonPath("$.id", is(correctExchange.getId())))
+                    .andExpect(jsonPath("$.value", is(correctExchange.getValue())));
+        }
+
+        for (Exchange incorrectExchange : incorrectExchanges) {
+            mockMvc.perform(post("/exchanges")
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .content(asJsonString(incorrectExchange)))
+                    .andExpect(status().isBadRequest());
+        }
+    }
+
+    @Test//finished
+    public void getExchangeTest() throws Exception {
+        addSampleValuesToDatabase();
+        correctExchanges.add(new Exchange(baseExchange, 1.0));
+
+        for (int i = 0; i < correctExchanges.size(); i++) {
+            for (int j = 0; j < correctExchanges.size(); j++) {
+                getExchangeTestingMethod(i, j);
+            }
+        }
+    }
+
+    @Test//finished
+    public void getAllExchangeTest() throws Exception {
+
+        for (int i = 0; i < correctExchanges.size(); i++) {
+            mockMvc.perform(get("/exchanges/all"))
+                    .andExpect(
+                            status().isOk())
+                    .andExpect(
+                            content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                    .andExpect(
+                            jsonPath("$", hasSize(correctExchanges.size())));
+        }
+
+    }
+
+    @Test//finished
+    public void deleteExchangeTest() throws Exception {
+
+        for (Exchange correctExchange : correctExchanges) {
+            mockMvc.perform(delete("/exchanges")
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .content(asJsonString(correctExchange)))
+                    .andExpect(status().isOk());
+        }
+        for (Exchange correctExchange : correctExchanges) {
+            mockMvc.perform(delete("/exchanges")
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .content(asJsonString(correctExchange)))
+                    .andExpect(status().isNotFound());
+        }
+        for (Exchange incorrectExchange : incorrectExchanges) {
+            mockMvc.perform(delete("/exchanges")
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .content(asJsonString(incorrectExchange)))
+                    .andExpect(status().isBadRequest());
+        }
+    }
+
+    //finished
+    private void getExchangeTestingMethod(int firstValue, int secondValue) throws Exception {
+
+        mockMvc.perform(get("/exchanges")
+                .param("FE", correctExchanges.get(firstValue).getId())
+                .param("SE", correctExchanges.get(secondValue).getId())
+        ).andExpect(status().isCreated())
+                .andExpect(
+                        jsonPath(
+                                "$.id",
+                                is(
+                                        correctExchanges.get(firstValue).getId() + "/" + correctExchanges.get(secondValue).getId()
+                                )))
+                .andExpect(
+                        jsonPath(
+                                "$.value",
+                                is(
+                                        firstValue != secondValue ?
+                                                BigDecimal.valueOf(correctExchanges.get(firstValue).getValue() / correctExchanges.get(secondValue).getValue())
+                                                        .setScale(3, RoundingMode.HALF_UP)
+                                                        .doubleValue()
+                                                : 1.0
+                                )));
+    }
+
+    //finished
+    private void addSampleValuesToDatabase() throws IncorrectInputDataException {
+        for (Exchange correctExchange : correctExchanges) {
+            if (correctExchange.getId().equals(baseExchange))
+                throw new IncorrectInputDataException(ExceptionHandlingMessages.BASE_EXCHANGE_MESSAGE);
+            exchangeRepository.save(correctExchange);
+        }
+    }
+
+    private static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
